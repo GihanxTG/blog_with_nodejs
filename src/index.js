978b29c2336e50d97ffeeaf1d31d9313d57376c0
GihanxTG <congnghia0802@email.com>
2024-04-29 20:07:57 +0700

    install handlebars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-// const handlebars = require('express-handlebars');
-// import { handlebars } from 'express-handlebars';
+const { engine } = require ('express-handlebars');
 const app = express();
 const port = 3000;
 
@@ -10,16 +9,14 @@ const port = 3000;
 app.use(morgan('combined'));
 
 //Template engine
-// app.engine('handlebars', handlebars());
-// app.set('view engine', 'handlebars');
-// app.set('views', path.join(__dirname, 'resources/views'));
-
-console.log('PATH: ', path.join(__dirname, 'views'));
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 
 app.get('/', (req, res) => {
   res.render('home');
-})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
